29f4d1803af5affbb1d70f2579020f09d221f481
Mukesh Sharma <sharmamukesh76@yahoo.com>
2020-06-27 16:56:21 +0530

    remove comma from description

--- nodejs.groovy
+++ nodejs.groovy
@@ -17,6 +17,6 @@ job('NodeJS-dev-api') {
         shell("aws s3 cp s3://ajnadeployment/cdev/backend_config/cdev.json  $WORKSPACE/config/")
         shell("zip -r $JOB_NAME-1.2.$BUILD_ID.zip * .[^.]*")
         shell("aws s3 cp $JOB_NAME-1.2.$BUILD_ID.zip s3://ajnabuildartifact")
-        shell("aws deploy create-deployment	--region us-east-1 --application-name ajna-cdev --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name ajna-cdev-api --description "Ajna dev Api deployment" --s3-location bucket=ajnabuildartifact,bundleType=zip,key=$JOB_NAME-1.2.$BUILD_ID.zip")
+        shell("aws deploy create-deployment	--region us-east-1 --application-name ajna-cdev --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name ajna-cdev-api --description Ajna dev Api deployment --s3-location bucket=ajnabuildartifact,bundleType=zip,key=$JOB_NAME-1.2.$BUILD_ID.zip")
     }
 }
